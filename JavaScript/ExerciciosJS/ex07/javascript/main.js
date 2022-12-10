6dc30294c1a8ac1aaccdf0acee11809fdd1ed00e
Washington Luiz De Sousa <washluiz_@hotmail.com>
2022-12-10 20:56:47 -0300

    js
    interagindo um pouco mais com javaScript

---
+++ JavaScript/ExerciciosJS/ex07/javascript/main.js
@@ -0,0 +1,18 @@
+
+function resultado() {
+    let name = prompt('Qual o nome do aluno: ');
+    let primeiraNota = Number(prompt(`Qual a primeira nota do aluno ${name}: `));
+    let segundaNota = Number(prompt(`Qual a segunda nota do aluno ${name}: `));
+    let mediaAtual = (primeiraNota + segundaNota) / 2;
+    let res = document.querySelector('section#info');
+    if (mediaAtual >= 7) {
+        res.innerHTML += `<p id="aprovado"><strong>Aprovado(a)</strong>!!</p>`;
+    } else if (mediaAtual >= 4) {
+        res.innerHTML += `<p id="recuperacao"><strong>Recuperação</strong>!</p>`
+    } else {
+        res.innerHTML += `<p id="reprovado"><strong>Reprovado(a)</strong>!</p>`;
+    }
+
+    res.innerHTML += `<p>Aluno(a): ${name} 1°Nota: ${primeiraNota} / 2°Nota: ${segundaNota} / média final ${mediaAtual}</p>`;
+}
+
---
+++ JavaScript/ExerciciosJS/ex08/script.js
@@ -0,0 +1,15 @@
+
+let res = document.querySelector('section#registrar');
+
+function action1() {
+    res.innerHTML += `<p>Clicou no primerio botão</p>`;
+}
+function action2() {
+    res.innerHTML += `<p>Clicou no segundo botão</p>`;
+}
+function action3() {
+    res.innerHTML += `<p>Clicou no terceiro botão</p>`;
+}
+function action4() {
+    res.innerHTML += `<p>Clicou no quarto botão</p>`;
+}
